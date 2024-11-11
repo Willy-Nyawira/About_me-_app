8d36eb9786577fc668954d53b14941f7b98d205c
Willy Nyawira <nyawiramwangi394@gmail.com>
2024-11-11 14:44:26 +0300

    About me app using Kotlin

---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
+
+//        findViewById<Button>(R.id.done_button).setOnClickListener {
+//            addNickname(it)
+        binding.doneButton.setOnClickListener {
+            addNickname(it) }
+        }
+
+    private fun addNickname(view:View){
+//        val editText=findViewById<EditText>(R.id.nickname_edit)
+//        val nicknameTextView=findViewById<TextView>(R.id.nickname_text)
+        binding.apply {this
+            nicknameText.text = binding.nicknameEdit.text
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        //hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+    }
+}
